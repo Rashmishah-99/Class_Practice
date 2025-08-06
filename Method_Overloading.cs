dd7997a4505e974b70fe840e04dee794286a2c34
rs889 <rs889@RASHMI_SHAH>
2025-08-06 10:16:31 +0530

    Add project files.

---
+++ Method_Overloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    internal class Method_Overloading
+    {
+         static int Add(int x, int y)
+        {
+            return x + y;
+        }
+       static int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public static void Main(string[] args)
+        {
+            int res1 = Add(5, 10);
+            Console.WriteLine("Result of Add: " + res1);
+            int res2 = Add(5, 10, 15);
+            Console.WriteLine("Result of Add: " + res2);
+        }
+    }
+}
